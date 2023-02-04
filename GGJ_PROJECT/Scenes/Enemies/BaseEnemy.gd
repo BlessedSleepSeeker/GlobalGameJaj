90d49730cd73c4b8b45ce05fdd6fca3939a162fc
Camille Gouneau <camille.gouneau@laposte.net>
2023-02-04 17:16:03 +0100

    Added Baseline UI for demo

--- GGJ_PROJECT/Scenes/Enemies/BaseEnemy.gd
+++ GGJ_PROJECT/Scenes/Enemies/BaseEnemy.gd
@@ -1,13 +1,14 @@
-extends RigidBody2D
+extends Area2D
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
-export(int) var HP := 3
-export(float) var MOVE_SPEED := 300.0
+export(int) var HP = 3
+export(float) var MOVE_SPEED = 300.0
 
+export(int) var DAMAGE = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,5 +20,6 @@ func _ready():
 #func _process(delta):
 #	pass
 
-func _on_BaseEnemy_body_shape_entered():
-	print('AAAAAAAA')
+func _on_BaseEnemy_body_entered(body: Node):
+	if body is Player:
+		body.damage(self, DAMAGE)
---
+++ GGJ_PROJECT/Scenes/UI/MainUI.gd
@@ -0,0 +1,19 @@
+extends CanvasLayer
+
+var player
+var state_machine
+var label
+
+var format_string = "X = %d\nY = %d\nDir = %d\nState = %s\nInput = [%f, %f]\nRotation = [%f]"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	player = get_parent().get_node("BaseRoom/Player")
+	state_machine = player.get_node("StateMachine")
+	label = $Margin/DebuggingDisplay
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	var state = state_machine.state.name
+	label.text = format_string % [player.position.x, player.position.y, player.rotation, state, player.v_direction.x, player.v_direction.y, player.v_rotation]
