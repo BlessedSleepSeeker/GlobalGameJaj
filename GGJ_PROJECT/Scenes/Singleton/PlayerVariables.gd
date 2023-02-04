56127e8911e822b36bdbd6629e0e8dcfa28b607c
Eindall <whitedragon43000@gmail.com>
2023-02-04 18:33:14 +0100

    Added missing file

---
+++ GGJ_PROJECT/Scenes/Singleton/PlayerVariables.gd
@@ -0,0 +1,20 @@
+extends Node
+
+export(int) var HP = 6
+export(int) var HP_MAX = 6
+export(float) var MOVE_SPEED = 500.0
+export(float) var ANGULAR_SPEED = PI * 4
+
+export(int) var ROLL_STARTUP = 2
+export(int) var ROLL_ACTIVE = 14
+export(int) var ROLL_RECOVERY = 3
+
+export(int) var ATTACK_SPEED = 18
+
+var v_direction := Vector2.ZERO
+var v_rotation
+var velocity := Vector2.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
