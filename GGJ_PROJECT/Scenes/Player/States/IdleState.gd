5d1c6af74b7224edba303f9b5ff58b41a8413a96
Adrien Keller <adrien.keller@epitech.eu>
2023-02-04 11:13:57 +0100

    init states
    Create initial states in folders

---
+++ GGJ_PROJECT/Scenes/Player/States/IdleState.gd
@@ -0,0 +1,13 @@
+# Idle.gd
+extends State
+
+# Upon entering the state, we set the Player node's velocity to zero.
+func enter(_msg := {}) -> void:
+	# We must declare all the properties we access through `owner` in the `Player.gd` script.
+	owner.velocity = Vector2.ZERO
+
+
+func update(delta: float) -> void:
+
+	if Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right") or Input.is_action_pressed("move_up") or Input.is_action_pressed("move_down"):
+		state_machine.transition_to("Run")
