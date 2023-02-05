b4a58c6818fc5e97ca605ffa2feab2e9733894d2
Camille Gouneau <camille.gouneau@laposte.net>
2023-02-05 10:57:41 +0100

    Added Scene Transition

--- GGJ_PROJECT/Scenes/Rooms/Generators/GeneratorDodgePuzzle.gd
+++ GGJ_PROJECT/Scenes/Rooms/Generators/GeneratorDodgePuzzle.gd
@@ -1,8 +1,7 @@
 extends Node
 
-var rng = RandomNumberGenerator.new()
-var seeding
 
+var seeding
 var game_state
 
 # Called when the node enters the scene tree for the first time.
@@ -11,5 +10,6 @@ func _ready():
 	game_state = get_node("/root/GameState")
 
 func act_one() -> BaseRoom:
+	game_state.current_room_type = "DodgePuzzle"
 	var demo = load("res://Scenes/Rooms/RoomTemplates/PuzzleRoom/DemoPuzzleRoom.tscn")
 	return demo.instance()
--- GGJ_PROJECT/Scenes/UI/HUD/MainUI.gd
+++ GGJ_PROJECT/Scenes/UI/HUD/MainUI.gd
@@ -4,11 +4,13 @@ onready var player_vars = get_node("/root/PlayerVariables")
 var player
 var state_machine
 var label
+var game_state
 
-var format_string = "X = %d\nY = %d\nDir = %d\nState = %s\nInput = [%f, %f]"
+var format_string = "X = %d\nY = %d\nDir = %d\nState = %s\nInput = [%f, %f]\nRoom %d : %s"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	game_state = get_node("/root/GameState")
 	player = get_parent().get_node("Player")
 	state_machine = player.get_node("StateMachine")
 	label = $Margin/RowSplit/DebuggingDisplay
@@ -17,5 +19,5 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	var state = state_machine.state.name
-	label.text = format_string % [player.position.x, player.position.y, player.rotation, state, player_vars.v_direction.x, player_vars.v_direction.y]
+	label.text = format_string % [player.position.x, player.position.y, player.rotation, state, player_vars.v_direction.x, player_vars.v_direction.y, game_state.current_room_number, game_state.current_room_type]
 
